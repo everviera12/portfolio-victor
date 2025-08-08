fd2397f2b1b60fd12cdd75e0183e19606ccf9bb4
everviera12 <evertigre10@gmail.com>
2025-08-08 13:24:26 -0600

    new keys

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -19,10 +19,10 @@ export default function ContactForm() {
 
         try {
             await emailjs.sendForm(
-                import.meta.env.EMAILJS_SERVICE_ID,
-                import.meta.env.EMAILJS_TEMPLATE_ID,
+                import.meta.env.VITE_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
                 formRef.current,
-                import.meta.env.EMAILJS_PUBLIC_KEY
+                import.meta.env.VITE_EMAILJS_PUBLIC_KEY
             );
             setSent(true);
             setTimeout(() => setSent(false), 3000);
